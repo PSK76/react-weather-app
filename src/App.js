be1bbf077d0bf65fed212992a97c6ab34868d89e
Priyanka <prettypri70@gmail.com>
2022-02-06 16:42:21 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        This was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/priyanka-ganesh-950629206/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Priyanka{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/PSK76/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open Sourced on GitHub
-        </a>
-        .
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/priyanka-ganesh-950629206/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Priyanka{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/PSK76/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
