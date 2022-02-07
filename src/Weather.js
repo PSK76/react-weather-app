ef101552bb959e4e1a06f3caedcd374558d7c6b4
Priyanka <prettypri70@gmail.com>
2022-02-06 19:46:21 -0700

    Added stylings

--- src/Weather.js
+++ src/Weather.js
@@ -6,15 +6,20 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-7">
+          <div className="col-9">
             <input
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
-          <div className="col-5">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,14 +28,19 @@ export default function Weather() {
         <li>Sunday 04:54</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://cdn-icons.flaticon.com/png/128/3222/premium/3222801.png?token=exp=1644193363~hmac=9ace378286bffefe1aadc205e4b97af8"
-            width="80"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="d-flex">
+            <img
+              src="https://cdn-icons.flaticon.com/png/128/3222/premium/3222801.png?token=exp=1644193363~hmac=9ace378286bffefe1aadc205e4b97af8"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
