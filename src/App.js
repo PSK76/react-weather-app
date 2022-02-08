05ab371e12b5e1a61c895799d68e6bf6ac1dbb7b
Priyanka <prettypri70@gmail.com>
2022-02-07 17:05:36 -0700

    Added the API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Edmonton" />
         <footer>
           This was coded by{" "}
           <a
